dab70b9b3acee82bf721629fdc831554457322a2
Robert Jordan <robert@parkside.app>
2020-04-09 15:18:19 -0700

    Versioning var.

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 resource "aws_s3_bucket" "this" {
     bucket = var.bucket
+    versioning {
+        enabled = var.enable_versioning
+    }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "bucket" {
     description = "Name of bucket to create"
 }
+
+variable "enable_versioning" {
+    description = "Enable versioning"
+    type = bool
+    default = false
+}
