289b095e15a51569771d562354f1d73ed9284b73
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2020-04-21 14:42:09 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "aws_s3_bucket" "this" {
-    bucket = var.bucket
-    versioning {
-        enabled = var.enable_versioning
-    }
-}
-
 resource "confluentcloud_environment" "test" {
   name = "parkside-test"
-}
-
-data "onepassword_group" "this" {
-    name = "exist-group"
 }
